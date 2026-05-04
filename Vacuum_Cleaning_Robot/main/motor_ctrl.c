75b4a8e82f558145eb2889454d89743f1c80eb4a
Mowah Chibuike <mctimmykings@gmail.com>
2026-05-04 10:54:56 +0000

    Refactor the motor init logic and structure

--- Vacuum_Cleaning_Robot/main/motor_ctrl.c
+++ Vacuum_Cleaning_Robot/main/motor_ctrl.c
@@ -2,14 +2,133 @@
 
 #define TAG     "MOTOR"
 
-static mcpwm_timer_handle_t timer = NULL;
-static mcpwm_cmpr_handle_t left_ctrl_grp = NULL;
-static mcpwm_cmpr_handle_t right_ctrl_grp = NULL;
+mcpwm_cmpr_handle_t left_ctrl_grp = {0};
+mcpwm_cmpr_handle_t right_ctrl_grp = {0};
 
+pcnt_unit_handle_t left_encoder = NULL;
+pcnt_unit_handle_t right_encoder = NULL;
 
-esp_err_t motors_init()
+static l298n_motor_ctx_t front_left = {0};
+static l298n_motor_ctx_t rear_left = {0};
+static l298n_motor_ctx_t front_right = {0};
+static l298n_motor_ctx_t rear_right = {0};
+
+esp_err_t pcnt_encoder_init(uint8_t chan_a, uint8_t chan_b, pcnt_unit_handle_t *out_handle)
 {
     esp_err_t err;
+
+    pcnt_unit_config_t unit_config = {
+        .high_limit = ENCODER_PCNT_HIGH_LIMIT,
+        .low_limit = ENCODER_PCNT_LOW_LIMIT,
+        .flags.accum_count = true, // enable counter accumulation
+    };
+    pcnt_unit_handle_t pcnt_unit = NULL;
+    err = pcnt_new_unit(&unit_config, &pcnt_unit);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not create new pcnt unit: %d", err);
+        return (err);
+    }
+
+
+    pcnt_glitch_filter_config_t filter_config = {
+        .max_glitch_ns = 1000,
+    };
+    err = pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not set glitch filter: %d", err);
+        return (err);
+    }
+
+
+    pcnt_chan_config_t chan_a_config = {
+        .edge_gpio_num = chan_a,
+        .level_gpio_num = chan_b,
+    };
+    pcnt_channel_handle_t pcnt_chan_a = NULL;
+    err = pcnt_new_channel(pcnt_unit, &chan_a_config, &pcnt_chan_a);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not create new channel: %d", err);
+        return (err);
+    }
+
+
+    pcnt_chan_config_t chan_b_config = {
+        .edge_gpio_num = chan_b,
+        .level_gpio_num = chan_a,
+    };
+    pcnt_channel_handle_t pcnt_chan_b = NULL;
+    err = pcnt_new_channel(pcnt_unit, &chan_b_config, &pcnt_chan_b);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not create new channel: %d", err);
+        return (err);
+    }
+
+
+    err = pcnt_channel_set_edge_action(pcnt_chan_a, PCNT_CHANNEL_EDGE_ACTION_DECREASE, PCNT_CHANNEL_EDGE_ACTION_INCREASE);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not set edge action on channel A: %d", err);
+        return (err);
+    }
+
+    err = pcnt_channel_set_level_action(pcnt_chan_a, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not set level action on channel A: %d", err);
+        return (err);
+    }
+
+    err = pcnt_channel_set_edge_action(pcnt_chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not set edge action on channel B: %d", err);
+        return (err);
+    }
+    
+    err = pcnt_channel_set_level_action(pcnt_chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not set level action on channel B: %d", err);
+        return (err);
+    }
+    
+    err = pcnt_unit_add_watch_point(pcnt_unit, ENCODER_PCNT_HIGH_LIMIT);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not add watch points: %d", err);
+        return (err);
+    }
+
+    err = pcnt_unit_add_watch_point(pcnt_unit, ENCODER_PCNT_LOW_LIMIT);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not add watch points: %d", err);
+        return (err);
+    }
+
+    err = pcnt_unit_enable(pcnt_unit);
+    err = pcnt_unit_clear_count(pcnt_unit);
+    err = pcnt_unit_start(pcnt_unit);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Could not start pcnt unit: %d", err);
+        return (err);
+    }
+
+    *out_handle = pcnt_unit;
+
+    return (ESP_OK);
+}
+
+esp_err_t motor_hardware_start(void)
+{
+    esp_err_t err;
+    mcpwm_timer_handle_t timer = NULL;
+    
 
     err = mcpwm_timer_init(&timer);
     if (err != ESP_OK)
@@ -51,8 +170,6 @@ esp_err_t motors_init()
         return (err);
     }
 
- 
-
     gpio_config_t gpio_cfg = {
         .pin_bit_mask = OUTPUT_PIN_MASK,
         .mode = GPIO_MODE_OUTPUT,
@@ -68,6 +185,115 @@ esp_err_t motors_init()
         return (err);
     }
 
+    err = pcnt_encoder_init(LEFT_ENCODER_CHAN_A, LEFT_ENCODER_CHAN_B, &left_encoder);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to setup left encoder");
+        return (err);
+    }
+
+    err = pcnt_encoder_init(RIGHT_ENCODER_CHAN_A, RIGHT_ENCODER_CHAN_B, &right_encoder);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to setup left encoder");
+        return (err);
+    }
+
+    return (ESP_OK);
+}
+
+esp_err_t single_motor_init(gpio_ctrl_t gpio_ctrl, pcnt_unit_handle_t encoder, l298n_motor_ctx_t *out_ctx)
+{
+    esp_err_t err;
+
+    out_ctx->gpio_ctrl.ctrl_handle = gpio_ctrl.ctrl_handle;
+    out_ctx->gpio_ctrl.gpio_ctrl_a = gpio_ctrl.gpio_ctrl_a;
+    out_ctx->gpio_ctrl.gpio_ctrl_b = gpio_ctrl.gpio_ctrl_b;
+
+    out_ctx->pcnt_encoder = encoder;
+
+    pid_ctrl_config_t pid_config = {
+        .init_param = {
+            .kp           = PID_KP,
+            .ki           = PID_KI,
+            .kd           = PID_KD,
+            .cal_type     = PID_CAL_TYPE_INCREMENTAL,
+            .max_output   = MCPWM_PERIOD_TICKS - 1,
+            .min_output   = 0,
+            .max_integral = 1000,
+            .min_integral = -1000,
+        }
+    };
+    err = pid_new_control_block(&pid_config, &out_ctx->pid_ctrl);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failure to setup pid ctrl for front left motor");
+        return (err);
+    }
+
+    out_ctx->last_count    = 0;
+    out_ctx->target_speed  = 0;
+    out_ctx->report_pulses = 0;
+    out_ctx->duty_tick_max = MCPWM_PERIOD_TICKS;
+
+    gpio_set_level(gpio_ctrl.gpio_ctrl_a, 0);
+    gpio_set_level(gpio_ctrl.gpio_ctrl_b, 0);
+
+    return (ESP_OK);
+}
+
+esp_err_t motors_init()
+{
+    esp_err_t err;
+
+    err = motor_hardware_start();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to start required motor hardware");
+        return (err);
+    }
+
+    gpio_ctrl_t gpio_ctrl_cfg = {
+        .ctrl_handle = left_ctrl_grp,
+        .gpio_ctrl_a = FRONT_LEFT_A,
+        .gpio_ctrl_b = FRONT_LEFT_B,
+    };
+    err = single_motor_init(gpio_ctrl_cfg, NULL, &front_left);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to setup front left motor: %d", err);
+        return (err);
+    }
+
+    gpio_ctrl_cfg.ctrl_handle = left_ctrl_grp;
+    gpio_ctrl_cfg.gpio_ctrl_a = REAR_LEFT_A;
+    gpio_ctrl_cfg.gpio_ctrl_b = REAR_LEFT_B;
+    err = single_motor_init(gpio_ctrl_cfg, left_encoder, &rear_left);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to setup rear left motor: %d", err);
+        return (err);
+    }
+
+    gpio_ctrl_cfg.ctrl_handle = right_ctrl_grp;
+    gpio_ctrl_cfg.gpio_ctrl_a = FRONT_RIGHT_A;
+    gpio_ctrl_cfg.gpio_ctrl_b = FRONT_RIGHT_B;
+    err = single_motor_init(gpio_ctrl_cfg, NULL, &front_right);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to setup front right motor: %d", err);
+        return (err);
+    }
+
+    gpio_ctrl_cfg.ctrl_handle = right_ctrl_grp;
+    gpio_ctrl_cfg.gpio_ctrl_a = REAR_RIGHT_A;
+    gpio_ctrl_cfg.gpio_ctrl_b = REAR_RIGHT_B;
+    err = single_motor_init(gpio_ctrl_cfg, right_encoder, &rear_right);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to setup front left motor: %d", err);
+        return (err);
+    }
 
     return (ESP_OK);
 }
@@ -86,56 +312,54 @@ void drive_motor(int dir, uint8_t pinA, uint8_t pinB)
 }
 
 
-void move_forward(uint8_t speed)
-{
-    // Adjust IN Pins
-    drive_motor(1, FRONT_LEFT_A, FRONT_LEFT_B);
-    drive_motor(1, REAR_LEFT_A, REAR_LEFT_B);
-    drive_motor(1, FRONT_RIGHT_A, FRONT_RIGHT_B);
-    drive_motor(1, REAR_RIGHT_A, REAR_RIGHT_B);
+// void move_forward(uint8_t speed)
+// {
+//     // Adjust IN Pins
+//     drive_motor(1, FRONT_LEFT_A, FRONT_LEFT_B);
+//     drive_motor(1, REAR_LEFT_A, REAR_LEFT_B);
+//     drive_motor(1, FRONT_RIGHT_A, FRONT_RIGHT_B);
+//     drive_motor(1, REAR_RIGHT_A, REAR_RIGHT_B);
 
-    // Drive PWM to PWM pins
-    esp_err_t err = mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
-    if (err != ESP_OK)
-        ESP_LOGE(TAG, "Moto is not moving o");
-    mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
-}
+//     // Drive PWM to PWM pins
+//     mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
+//     mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
+// }
 
-void move_backward(uint8_t speed)
-{
-    // Adjust IN Pins
-    drive_motor(-1, FRONT_LEFT_A, FRONT_LEFT_B);
-    drive_motor(-1, REAR_LEFT_A, REAR_LEFT_B);
-    drive_motor(-1, FRONT_RIGHT_A, FRONT_RIGHT_B);
-    drive_motor(-1, REAR_RIGHT_A, REAR_RIGHT_B);
+// void move_backward(uint8_t speed)
+// {
+//     // Adjust IN Pins
+//     drive_motor(-1, FRONT_LEFT_A, FRONT_LEFT_B);
+//     drive_motor(-1, REAR_LEFT_A, REAR_LEFT_B);
+//     drive_motor(-1, FRONT_RIGHT_A, FRONT_RIGHT_B);
+//     drive_motor(-1, REAR_RIGHT_A, REAR_RIGHT_B);
 
-    // Drive PWM to PWM pins
-    mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
-    mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
-}
+//     // Drive PWM to PWM pins
+//     mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
+//     mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
+// }
 
-void turn_left(uint8_t speed)
-{
-    // Adjust IN Pins
-    drive_motor(-1, FRONT_LEFT_A, FRONT_LEFT_B);
-    drive_motor(-1, REAR_LEFT_A, REAR_LEFT_B);
-    drive_motor(1, FRONT_RIGHT_A, FRONT_RIGHT_B);
-    drive_motor(1, REAR_RIGHT_A, REAR_RIGHT_B);
+// void turn_left(uint8_t speed)
+// {
+//     // Adjust IN Pins
+//     drive_motor(-1, FRONT_LEFT_A, FRONT_LEFT_B);
+//     drive_motor(-1, REAR_LEFT_A, REAR_LEFT_B);
+//     drive_motor(1, FRONT_RIGHT_A, FRONT_RIGHT_B);
+//     drive_motor(1, REAR_RIGHT_A, REAR_RIGHT_B);
 
-    // Drive PWM to PWM pins
-    mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
-    mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
-}
+//     // Drive PWM to PWM pins
+//     mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
+//     mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
+// }
 
-void turn_right(uint8_t speed)
-{
-    // Adjust IN Pins
-    drive_motor(1, FRONT_LEFT_A, FRONT_LEFT_B);
-    drive_motor(1, REAR_LEFT_A, REAR_LEFT_B);
-    drive_motor(-1, FRONT_RIGHT_A, FRONT_RIGHT_B);
-    drive_motor(-1, REAR_RIGHT_A, REAR_RIGHT_B);
+// void turn_right(uint8_t speed)
+// {
+//     // Adjust IN Pins
+//     drive_motor(1, FRONT_LEFT_A, FRONT_LEFT_B);
+//     drive_motor(1, REAR_LEFT_A, REAR_LEFT_B);
+//     drive_motor(-1, FRONT_RIGHT_A, FRONT_RIGHT_B);
+//     drive_motor(-1, REAR_RIGHT_A, REAR_RIGHT_B);
 
-    // Drive PWM to PWM pins
-    mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
-    mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
-}
+//     // Drive PWM to PWM pins
+//     mcpwm_set_grp_duty_cycle(left_ctrl_grp, speed);
+//     mcpwm_set_grp_duty_cycle(right_ctrl_grp, speed);
+// }
--- Vacuum_Cleaning_Robot/main/motor_ctrl.h
+++ Vacuum_Cleaning_Robot/main/motor_ctrl.h
@@ -3,25 +3,103 @@
 
 #include "mcpwm_ctrl.h"
 #include <inttypes.h>
+#include "driver/pulse_cnt.h"
+#include "pid_ctrl.h"
 
-#define FRONT_LEFT_A        25
-#define FRONT_LEFT_B        33
+#define FRONT_LEFT_A        33
+#define FRONT_LEFT_B        25
 #define FRONT_LEFT_PWM      32
 
-#define REAR_LEFT_A         14
-#define REAR_LEFT_B         27
-#define REAR_LEFT_PWM       13
+#define REAR_LEFT_A         27
+#define REAR_LEFT_B         14
+#define REAR_LEFT_PWM       12
 
 #define FRONT_RIGHT_A       19
 #define FRONT_RIGHT_B       18
 #define FRONT_RIGHT_PWM     23
 
-#define REAR_RIGHT_A        12
+#define REAR_RIGHT_A        13
 #define REAR_RIGHT_B        15
 #define REAR_RIGHT_PWM      4
 
 #define OUTPUT_PIN_MASK     ((1ULL << FRONT_LEFT_A) | (1ULL << FRONT_LEFT_B) | (1ULL << REAR_LEFT_A) | (1ULL << REAR_LEFT_B) | (1ULL << FRONT_RIGHT_A) | (1ULL << FRONT_RIGHT_B )| (1ULL << REAR_RIGHT_A) | (1ULL << REAR_RIGHT_B))
 
+#define LEFT_ENCODER_CHAN_A     34
+#define LEFT_ENCODER_CHAN_B     35
+
+#define RIGHT_ENCODER_CHAN_A    39
+#define RIGHT_ENCODER_CHAN_B    36
+
+#define ENCODER_PPR             275
+#define WHEEL_CIRCUM            204.2f // mm
+#define PULSES_PER_REV          (WHEEL_CIRCUM / ENCODER_PPR)
+
+#define ENCODER_PCNT_HIGH_LIMIT   1000
+#define ENCODER_PCNT_LOW_LIMIT    -1000
+
+#define PID_KP                  1.0f
+#define PID_KI                  0.0f
+#define PID_KD                  0.0f
+#define PID_LOOP_PERIOS_MS      10
+
+typedef struct
+{
+    mcpwm_cmpr_handle_t ctrl_handle;
+    uint8_t gpio_ctrl_a;
+    uint8_t gpio_ctrl_b;
+} gpio_ctrl_t;
+
+typedef struct
+{
+    gpio_ctrl_t gpio_ctrl;
+
+    pcnt_unit_handle_t pcnt_encoder;
+
+    pid_ctrl_block_handle_t pid_ctrl;
+    
+    int32_t                 last_count;
+    int32_t                 report_pulses; 
+    float                   target_speed;
+    uint32_t                duty_tick_max;
+} l298n_motor_ctx_t;
+
+typedef enum {
+    MOTION_STATE_IDLE,
+    MOTION_STATE_MOVING,
+    MOTION_STATE_DECELERATING,
+    MOTION_STATE_DONE,
+} motion_state_t;
+
+typedef struct {
+    // Pointers to motors
+    l298n_motor_ctx_t         *front_left_motor;
+    l298n_motor_ctx_t         *front_right_motor;
+    l298n_motor_ctx_t         *rear_left_motor;
+    l298n_motor_ctx_t         *rear_right_motor;
+
+    // Motion state machine
+    motion_state_t      state;
+
+    // Distance tracking
+    int32_t             left_start_count;
+    int32_t             right_start_count;
+    int32_t             target_counts;       // in encoder counts
+
+    // Speed
+    float               base_speed;          // pulses/10ms
+    float               min_speed;           // floor during deceleration
+
+    // Deceleration
+    int32_t             decel_threshold;     // counts before target to start slowing
+
+    // Straight line correction
+    float               correction_gain;
+
+    // Completion signal to mission layer
+    TaskHandle_t        waiting_task;        // task to notify on completion
+} motion_ctx_t;
+
+esp_err_t pcnt_encoder_init(uint8_t chan_a, uint8_t chan_b, pcnt_unit_handle_t *out_handle);
 esp_err_t motors_init();
 void move_forward(uint8_t speed);
 void move_backward(uint8_t speed);
